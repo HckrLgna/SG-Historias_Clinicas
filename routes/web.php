e5e8595af2b27498489274d7d064d09be5c6cb54
Angel Blanco <ablanco837@gmail.com>
2022-04-26 12:21:17 -0400

    resolvemos problemas del frontend

--- routes/web.php
+++ routes/web.php
@@ -25,19 +25,22 @@ Route::get('home',function (){
 
 Auth::routes(['verify' => true]);
 
-Route::group(['middleware'=>['auth'],'as' => 'backoffice.'],function (){
-    Route::get('admin','App\Http\Controllers\AdminController@show')->name('admin.show');
-    Route::resource('user','App\Http\Controllers\UserController');
+Route::group(['middleware' => ['cors']], function () {
+    Route::group(['middleware'=>['auth'],'as' => 'backoffice.'],function (){
+        Route::get('admin','App\Http\Controllers\AdminController@show')->name('admin.show');
+        Route::resource('user','App\Http\Controllers\UserController');
 
-    Route::get('user/{user}/assign_role','App\Http\Controllers\UserController@assign_role')->name('user.assign_role');
-    Route::post('user/{user}/role_assignment','App\Http\Controllers\UserController@role_assignment')->name('user.role_assignment');
+        Route::get('user/{user}/assign_role','App\Http\Controllers\UserController@assign_role')->name('user.assign_role');
+        Route::post('user/{user}/role_assignment','App\Http\Controllers\UserController@role_assignment')->name('user.role_assignment');
 
-    Route::get('user/{user}/assign_permission','App\Http\Controllers\UserController@assign_permission')->name('user.assign_permission');
-    Route::post('user/{user}/permission_assignment','App\Http\Controllers\UserController@permission_assignment')->name('user.permission_assignment');
+        Route::get('user/{user}/assign_permission','App\Http\Controllers\UserController@assign_permission')->name('user.assign_permission');
+        Route::post('user/{user}/permission_assignment','App\Http\Controllers\UserController@permission_assignment')->name('user.permission_assignment');
 
-    Route::resource('role','App\Http\Controllers\RoleController');
-    Route::resource('permission','App\Http\Controllers\PermissionController');
+        Route::resource('role','App\Http\Controllers\RoleController');
+        Route::resource('permission','App\Http\Controllers\PermissionController');
+    });
 });
+
 
 Route::group(['as'=>'frontoffice.'], function (){
    Route::get('profile','App\Http\Controllers\UserController@profile')->name('user.profile');
